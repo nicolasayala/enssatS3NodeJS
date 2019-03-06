c44545a27e816ed7abbdf51511ef2c378253d338
Nicolas Ayala <nayala@enssat.fr>
2019-03-06 11:25:11 +0100

    added pug render engine

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Salut!');
+  res.render('form');
 });
 
 module.exports = router;
