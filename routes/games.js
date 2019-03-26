83e4a15c4da17083183efaa125ba63e1fcfe47cf
Jules Lagarde <jules.lagarde@gmail.com>
2019-03-26 16:58:57 +0100

    update get,post for /game/scores

--- routes/games.js
+++ routes/games.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sessionChecker = require('../utils/session-checker');
 const mongoose = require('mongoose');
 const Game = mongoose.model('Game');
+const User = mongoose.model('User');
 const router = express.Router();
 
 router.get('/', sessionChecker.filterLoggedOut, (req, res) => {
@@ -14,18 +15,57 @@ router.get('/', sessionChecker.filterLoggedOut, (req, res) => {
 });
 
 router.get('/scores', sessionChecker.filterLoggedOut, (req, res) => {
-      if(req.params.game) {
-        Game.findOne({
-          name: req.params.game
-        })
+    console.log("req query ->", req.query);
+    if (!req.query.hasOwnProperty("game")) {
+        res.redirect("/games");
+    }
+    Game.findOne({name: req.query.game})
         .then((game) => {
-            res.render('scores', {title: 'Tableau des scores ', game: game});
+            if(game)
+                res.render('scores', {title: 'Tableau des scores ', game: game});
+            else
+                res.render('scores', {title: 'Game "' + req.query.game + '" not found', game: null});
         })
         .catch(() => {
-          res.send('Sorry! Something went wrong.');
+            res.send('Sorry! Something went wrong.');
         });
-      }else
-        res.render('scores', {title: 'Game "'+req.params.game+'" not found', game: null});
 });
+
+router.post('/scores', sessionChecker.filterLoggedOut, (req, res) => {
+    console.log("req body ->", req.body);
+    if (!(req.body.hasOwnProperty("new_score") && req.query.hasOwnProperty("game"))) {
+        res.sendStatus(201);
+        return;
+    }
+    let user = req.session.user;
+    User.findOne({email:req.session.user.email})
+        .then((user)=>{
+            update_highscore(user, req.query.game, req.body.new_score);
+            req.session.user=user;
+            res.sendStatus(200);
+        })
+        .catch(() => {
+            res.send('Sorry! Something went wrong.');
+        });
+});
+
+function update_highscore(user, game, new_score) {
+    let hs = null;
+    for (hs of user.highscores) {
+        if (hs.game == game) {
+            break;
+        }
+    }
+    if (hs == null) {
+        hs = {game: game, highscore: 0};
+        user.highscores.push(hs);
+        console.log("should have new score");
+    }
+    if (hs.highscore < parseInt(new_score)) {
+        hs.highscore = parseInt(new_score);
+        console.log("new highscore ", new_score, user.email);
+        user.save();
+    }
+}
 
 module.exports = router;
