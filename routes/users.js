10818dd29011abd9656d9d41f94c94405fd83e13
Nicolas Ayala <nayala@enssat.fr>
2019-03-27 17:01:36 +0100

    Admins can now ban or promote users with Users route (/users)

--- routes/users.js
+++ routes/users.js
@@ -5,17 +5,29 @@ const User = mongoose.model('User');
 const router = express.Router();
 
 router.get('/', sessionChecker.filterUser, (req, res) => {
-  User.find()
-    .then((users) => {
-      res.render('users', { title: 'Listing users', users, admin:true });
-    })
-    .catch(() => { res.send('Sorry! Something went wrong.'); });
+    User.find()
+        .then((users) => {
+            res.render('users', {
+                title: 'Listing users',
+                users,
+                admin: true
+            });
+        })
+        .catch(() => {
+            res.send('Sorry! Something went wrong.');
+        });
 });
 
 router.post('/', (req, res) => {
-    console.log("body")
-    console.log(req.body);
-    res.render('users', { title: 'Listing users', users:[], admin:true });
+    User.findById(req.body.id, (err, user) => {
+        if (user) {
+            user.isBanned = req.body.hasOwnProperty('banned') && (req.body.banned == 'on')
+            user.isAdmin = req.body.hasOwnProperty('admin') && (req.body.admin == 'on')
+            user.save().then(() => res.redirect('/users'))
+        } else {
+            res.send('Sorry! Something went wrong.');
+        }
+    });
 });
 
 module.exports = router;
