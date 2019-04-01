0ed6644de158dfd64a2fee558e663d6e40d922ca
Jules Lagarde <jules.lagarde@gmail.com>
2019-04-01 14:10:13 +0200

    games comments

--- routes/games.js
+++ routes/games.js
@@ -1,3 +1,12 @@
+/**
+ *
+ * games.js
+ *
+ * Route /games
+ * handle all request on /games and /games/scores
+ *
+ */
+
 const express = require('express');
 const sessionChecker = require('../utils/session-checker');
 const mongoose = require('mongoose');
@@ -7,42 +16,54 @@ const Highscore = mongoose.model('Highscore');
 const router = express.Router();
 
 router.get('/', sessionChecker.filterLoggedOut, (req, res) => {
+    //search all the games in the database
     Game.find()
         .then((games) => {
-            User.findOne({email:req.session.user.email})
-                .populate([{path:"highscores", select:"game value"},{path:"highscores", populate:{path:"game", select:"name"}}])
-                .then((user)=>{
-                    if(user==null) {
+            User.findOne({email: req.session.user.email})
+                .populate([{path: "highscores", select: "game value"}, {
+                    path: "highscores",
+                    populate: {path: "game", select: "name"}
+                }])
+                //replace id by object for user.highscores[i].value and user.highscores[i].game.name
+                .then((user) => {
+                    if (user == null) {
                         res.send('Sorry! Something went wrong. (login)');
                         return;
                     }
-                    let personalHighscores={};
-                    //console.log(user);
-                    for(let hs of user.highscores){
-                        personalHighscores[hs.game.name]=hs.value;
+                    let personalHighscores = {};
+                    for (let hs of user.highscores) {
+                        personalHighscores[hs.game.name] = hs.value;
                     }
-                    //console.log("personnalHighscore : ", personalHighscores);
                     res.render('games',
-                        { title: 'Listing games', games: games, admin: req.session.user.isAdmin, personalHighscores });
+                        {title: 'Listing games', games: games, admin: req.session.user.isAdmin, personalHighscores});
                 });
         })
-        .catch(() => { res.send('Sorry! Something went wrong.'); });
+        .catch(() => {
+            res.send('Sorry! Something went wrong.');
+        });
 });
 
 router.get('/scores', sessionChecker.filterLoggedOut, (req, res) => {
-    console.log("g req query ->", req.query);
     if (!req.query.hasOwnProperty("game")) {
         res.redirect("/games");
         return;
     }
+    //find the game with the name in the url (/games?game=Tetris)
     Game.findOne({name: req.query.game})
-        .populate([{path:"highscores", select:"user value"},{path:"highscores", populate:{path:"user", select:"isBanned isAdmin email"}}])
+        .populate([{path: "highscores", select: "user value"}, {
+            path: "highscores",
+            populate: {path: "user", select: "isBanned isAdmin email"}
+        }])
+        //replace id by object for game.highscores[i].value and game.highscores[i].user.(isBanned/isAdmin/email)
         .then((game) => {
-            console.log("gJSON", JSON.stringify(game));
-            if(game)
+            if (game)
                 res.render('scores', {title: 'Tableau des scores ', game: game, admin: req.session.user.isAdmin});
             else
-                res.render('scores', {title: 'Game "' + req.query.game + '" not found', game: null, admin: req.session.user.isAdmin});
+                res.render('scores', {
+                    title: 'Game "' + req.query.game + '" not found',
+                    game: null,
+                    admin: req.session.user.isAdmin
+                });
         })
         .catch(() => {
             res.send('Sorry! Something went wrong.');
@@ -50,52 +71,58 @@ router.get('/scores', sessionChecker.filterLoggedOut, (req, res) => {
 });
 
 router.post('/scores', sessionChecker.filterLoggedOut, (req, res) => {
-    console.log("p req body ->", req.body);
-    console.log("p req query ->", req.query);
+    //console.log("p req body ->", req.body);
+    //console.log("p req query ->", req.query);
     if (!(req.body.hasOwnProperty("new_score") && req.query.hasOwnProperty("game"))) {
         res.sendStatus(222);
         return;
     }
-    Game.findOne({name:req.query.game}, (err, game)=>{
-        if(err){
-            console.log("err",err);
+    Game.findOne({name: req.query.game}, (err, game) => {
+        if (err) {
+            console.log("err", err);
             return;
         }
-            User.findOne({email: req.session.user.email})
-                .then((user)=>{
-                    if(!game){
-                        res.send('Game "' + req.query.game + '" not found');
-                        console.error('post : Game "' + req.query.game + '" not found');
-                    }else if(!user){
-                        res.send('user not found');
-                        console.error('post : user not found', req.session.user.email)
-                    }else{
-                        update_highscore(game, user, req.body.new_score, ()=>{
-                            console.log('score updated');
-                            res.sendStatus(201);
-                        })
-                    }
-                    req.session.user=user;
-                })
-                .catch(() => {
-                    res.send('Sorry! Something went wrong. user');
-                });
-        })
-        // .catch(() => {
-        //     res.send('Sorry! Something went wrong. game');
-        // });
+        User.findOne({email: req.session.user.email})
+            .then((user) => {
+                if (!game) {
+                    res.send('Game "' + req.query.game + '" not found');
+                    console.error('post : Game "' + req.query.game + '" not found');
+                } else if (!user) {
+                    res.send('user not found');
+                    console.error('post : user not found', req.session.user.email)
+                } else {
+                    update_highscore(game, user, req.body.new_score, () => {
+                        console.log('score updated');
+                        res.sendStatus(201);
+                    })
+                }
+                req.session.user = user;
+            })
+            .catch(() => {
+                res.send('Sorry! Something went wrong. user');
+            });
+    })
 });
 
+
+/**
+ * Update the highscore for a user on a game
+ * @param {Object} game Mongoose (Game) model for a specific game
+ * @param {Object} user Mongoose (User) model for a specific game
+ * @param {Number} new_score the new value of highscores
+ * @param callback function which will be called when done
+ */
 function update_highscore(game, user, new_score, callback) {
     console.log("highscore", game.highscore);
-    if(game.highscore<new_score){
-        game.highscore=new_score;
+    if (game.highscore < new_score) {
+        game.highscore = new_score;
         game.save();
     }
-    Highscore.findOne({game:game._id, user:user._id})
-        .then((hs)=> {
+    Highscore.findOne({game: game._id, user: user._id})
+        .then((hs) => {
+            //if the player has not played the game before we need to create an entry
             if (hs == null) {
-                hs = new Highscore({game: game._id, user:user._id, value: 0});
+                hs = new Highscore({game: game._id, user: user._id, value: 0});
                 //hs.save();//needed to generate the _id ?? --no it's done on object creation
                 user.highscores.push(hs._id);
                 user.save();
@@ -103,10 +130,10 @@ function update_highscore(game, user, new_score, callback) {
                 game.save();
                 console.log("should have new score ", hs._id);
             }
-            if (hs.value <= parseInt(new_score)) {
-              hs.value = parseInt(new_score);
-              console.log("new highscore ", new_score, user.email);
-              hs.save();
+            if (hs.value < parseInt(new_score)) {
+                hs.value = parseInt(new_score);
+                console.log("new highscore ", new_score, user.email);
+                hs.save();
             }
             //console.log(hs, game, user);
             callback();
